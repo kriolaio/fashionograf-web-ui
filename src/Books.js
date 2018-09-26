b6ff963e88b9a24db206cf8c75ee7f0f3dd5de7b
ethems <furkan.ozdeslik@outlook.com>
2018-09-26 13:10:07 -0700

    create apollo client

---
+++ src/Books.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const Books = () => (
+  <Query
+    query={gql`
+      {
+        books {
+          title
+          author
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.books.map(({ title, author }) => (
+        <div key={title}>
+          <p>{`${title}: ${author}`}</p>
+        </div>
+      ));
+    }}
+  </Query>
+);
+
+export default Books;
